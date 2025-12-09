006c6f7bb95dac16906b7187718e9ad88b47fda2
turansmv004 <turansmv@gmail.com>
2025-12-09 20:56:02 +0400

    slowly

--- bot.ts
+++ bot.ts
@@ -95,4 +95,4 @@ bot.launch().then(() => {
 });
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+process.once('SIGTERM', () => bot.stop('SIGTERM')); 
